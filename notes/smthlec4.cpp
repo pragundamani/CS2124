902a9ea88155f9006956e12dfa52247a26e6b5ee
para <damanipragun@proton.me>
2026-02-02 12:10:30 -0500

    updates to lec4

---
+++ notes/smthlec4.cpp
@@ -0,0 +1,66 @@
+// lec 4 notes
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// in struct everything is by default public
+// in class everything by default is private
+class Person {
+public:
+    // Person(const string& aName)/*this spot is called the initialization list*/ {
+        //all non primitives are constructed at that point ^^
+    //if you dont make an initialization list all non-primitives and contructed
+
+    // Person(const string& aName) : name(aName)/*init list*/{
+    Person(const string& name) : name(name){
+        //no return type on constructors
+        // name = aName;
+
+    }
+
+    //a method is a function defined within an object
+    void eating() const { //no need to repeat type within method's name like  personEating
+        cout << name << " eating\n";
+    }
+    // void setName(const string& aName){
+    //     name = aName;
+    // }
+
+    //by putting keyword const compiler knows its safe
+    const string& getName() const { return name; } //no compile since called on an immutable object
+    // if there is no reason to modify anything within a method mark it is as const
+
+private:
+    string name;
+};
+
+void personEating(const Person& aPerson){
+    // cout << aPerson.name << " eating\n";
+    cout << aPerson.getName() << " eating\n";
+}
+
+ostream& operator<<(ostream& os /*any name*/, const Person& aPerson){
+    os << "Person: " << aPerson.getName();
+    return os;
+}
+
+int main(){
+   Person john("John");
+//   john.name = "John";
+//   personEating(john);
+//   john.setName("John"); not allowing names to be changed
+
+  //john.personEating(john) the way we would have had to do it
+  john.eating();
+
+  personEating(john);
+
+  // Person mary;
+
+  // operator<<(cout, john);
+  (cout << john) << endl;
+
+  return 0;
+}
